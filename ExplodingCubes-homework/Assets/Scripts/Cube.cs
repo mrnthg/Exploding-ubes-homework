e6ed9d1e350e6ea056fa1ce7293530233b168c43
MoranThug <moranthug@gmail.com>
2024-05-06 15:31:56 +1000

    v 0.3

--- ExplodingCubes-homework/Assets/Scripts/Cube.cs
+++ ExplodingCubes-homework/Assets/Scripts/Cube.cs
@@ -9,22 +9,4 @@ public class Cube : MonoBehaviour
         _explosion—ontroller.SpawnNewCubes(gameObject);
         Destroy(gameObject);
     }
-
-    //private void FixedUpdate()
-    //{
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //        RaycastHit hitInfo;
-    //        if (Physics.Raycast(ray, out hitInfo))
-    //        {
-    //            Debug.Log("smt");
-    //            if (hitInfo.collider.gameObject.tag == "Cubes")
-    //            {
-    //                Debug.Log("tag");
-
-    //            }
-    //        }
-    //    }
-    //}
 }
